8de08d8ffc4fd501692355f16ed2d62b729ac49c
Suraj Govind <surajgovind332000@gmail.com>
2021-02-13 23:51:05 +0530

    migrate to heroku

--- src/constants/firebaseService.js
+++ src/constants/firebaseService.js
@@ -1,7 +1,7 @@
 const serviceJSON = {
   type: "service_account",
   project_id: "web-mark-304119",
-  private_key_id: process.env.FIREBASE_SERVICE_PRIVATE_KEY_ID,
+  private_key_id: process.env.REACT_APP_FIREBASE_SERVICE_PRIVATE_KEY_ID,
   private_key:
     "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCybrYzCxMyi00P\npAHkjXwYcyM+0HwdyDHQgGOtevrq81YD5nzrcgfoZqZgImvD1H/Rk/TYGVDo0WGX\neagW7m3AP44tXev6V+ccFBwzL3pbIm5WXLgxMNW/QM6a4e1Lj+9GUYkTOLnXCTmC\nB/+EsdG5MjadQJdT1oFJnAAxCawezNIiuMy4FHJn+OYrdahMtWmlNCaTdYiymCyE\nPTZb58MypwsCQorGTqETT7sZEWSvrSuZwwJeLVakHdWEdIorqDsZO76VMCnJoNdl\nYmaC8ILqVNCh9F+7KOirT6VJnzK8OyQgiGPWfFEYjlavfcUYzoSAjXhgDV7UDOVn\nwooWgCRNAgMBAAECggEACCRaJwkd1qQZn5mVxoaYP4tdc9vSlg9RCpkZM0zVz0vM\nRIBLefjpmHa6p89aSITVUkxkk0nQcddAE0fnXiiCFJAx3isXfgRt1M+sG7/SXv86\ntrEjbIc0cbY010dw6w5F1lkZHqnrfP78Q003ISh2D/PVHybX4Oj8XE4EijhNRNhz\nQJXss5EUqthAyYwbmjIhfdfIppxmM/QRojkT5Gs0tgeRuU09wqpzmO/7xW4FPr57\neY+cmwGvJeiEMPCW/GYaD4TzIdg17p/knCyWPHZ3B9WsVFuFoPYElGYB4Qn67lBv\nxsBBuf/Gx+gfWFPBJqtb3WsCpwed5z5h3ZKIuhct8QKBgQDj8SxdcmPRr6UO2s3l\nJ3bdb6Epvl0dP9AOXnx5H5IwxCXCNlqDklH5RfrNmXkSAF0HOIoB1TGN2fvjMRnj\nWhwTvN3yIQXyc0rJww5cOq/OdX02jdtXJRZnZXzf/ceSSildxlFlEdA9ApD8n69V\noqBJa1Ic3x0e/IE30BxhnBKUNQKBgQDIZWjiXjv4xUK1ecEhlX6bRyRuGJcWXsUS\nRCrLD6PuhZgeeeZ7YE/XJpw35WWAdeVU9OIDhWf5nnXUlPIij4n5bHWKeGpwRz3v\nft8Gz55xOzaF6oG6miA9sfof/XvGmZZTU6nK6bIh5TQTA69R70RmPyVZ3630Wc+j\nfrQ25FQiuQKBgQDdQr5N9zvtzxHp2XsDdctsJ2/JlX/NZKQRTqaE7KB7terxGtHB\n5casscFC0jczwiaLW9aa8NepWm+nmYIHrvCSOEM5RdUAA+oU78KAxHbjciiZTBod\n/WJB+8G7rByNZG/DFz7rXN9GWbdjgZ4xAiuVGRgb7nfWHEVSaDEkjtt6AQKBgQCP\nSVNxaIfhHMrSD7i0VrwsMvYtco+1gxXONceTYh+JESTvxaGzXe44nHbe0Lt+2s2b\nrW1drynlJhSCZ+2qIJA7M/3ngKo6lfYHJOQ7boCP3hLiNgeEcWfUNUnvTcQPAQqn\nkg6589dWvRg68EuPflGd+lCurm0nZoA9Gv+osCIooQKBgBwn7pJ9qf+nDnTDIgRX\npJ1WcxCngTFCaNRALCHGFiRz+FxSLtMEcOkMsitHxxh01nmF/fY/ypIVJ9PbvraV\nraAGHJQIvHgtgWPtRG39zxDR5CikI+27bjT1TDZeyG9Hp3j00gPBvC3o2g9BttGW\nK1nFJ+JkxkxcuEY6jHD6xBbb\n-----END PRIVATE KEY-----\n",
   client_email:
@@ -14,4 +14,4 @@ const serviceJSON = {
     "https://www.googleapis.com/robot/v1/metadata/x509/web-mark-vercel-serverless%40web-mark-304119.iam.gserviceaccount.com",
 };
 
-export default serviceJSON;
+module.exports = serviceJSON;
